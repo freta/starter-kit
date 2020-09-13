75f96d23fb77304ec1d0b5b1f88c6bb574639247
freta <[itsmefruta@gmail.com]>
2020-09-13 23:24:31 +0300

    Automation, transpiling, boundling ..

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,2 @@
+import chalk from "chalk";
+console.log(chalk.green("strting app in the dev mode..."));
---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+/*import path from "path";
+import webpack from "webpack";
+
+export default {
+  debug: true,
+  devtool: "inline-source-map",
+  noInfo: false,
+  entry: [path.resolve(__dirname, "src/index")],
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "src"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ["babel-loader"] },
+      { test: /\.css$/, loaders: ["style", "css"] },
+    ],
+  },
+};*/
